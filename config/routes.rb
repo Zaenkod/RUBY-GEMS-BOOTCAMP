0947d4abfb301b2fe408ceffb8ddeb598206953b
Cedric Ruault <cedric.ruault215@gmail.com>
2023-11-02 16:42:51 +0100

    first routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  # get 'static_pages/landing_page'
+  get 'static_pages/privacy_policy'
   devise_for :users
-  root to: "pages#home"
+  # root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'static_pages#landing_page'
+  get 'privacy_policy', to: 'static_pages#privacy_policy'
 end
