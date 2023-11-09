b3d9d35b2de46897418c50aedf2df581363e61b4
Cedric Ruault <cedric.ruault215@gmail.com>
2023-11-09 17:30:30 +0100

    navbar home + style courses

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  # get 'home/index'
   # get 'static_pages/landing_page'
   get 'static_pages/privacy_policy'
   devise_for :users
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "courses#index"
+  root "home#index"
   # root 'static_pages#landing_page'
   get 'privacy_policy', to: 'static_pages#privacy_policy'
 end
