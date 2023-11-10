3d99415ac86aa3bd032cb5a7ec890e1c0771854a
Cedric Ruault <cedric.ruault215@gmail.com>
2023-11-10 10:16:29 +0100

    add friendly_id

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  # get 'users/index'
   # get 'home/index'
   # get 'static_pages/landing_page'
   get 'static_pages/privacy_policy'
   devise_for :users
   resources :courses
+  resources :users, only: [:index]
   # root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
